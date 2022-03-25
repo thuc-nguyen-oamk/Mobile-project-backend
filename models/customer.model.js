e3bb067a2c0b9121f6b3c753b925636266add5f6
t0dida00 <t0dida00@students.oamk.fi>
2022-03-25 23:12:28 +0200

    working 25/3

--- models/customer.model.js
+++ models/customer.model.js
@@ -4,5 +4,19 @@ module.exports = {
   getAllCustomer: () => {
     return db.load(`select * from ${TABLE_NAME}`);
   },
-
+  getCustomerByID: (customer_id) => {
+    return db.load(`select * from ${TABLE_NAME} where customer_id = ${customer_id}`);
+  },
+  updateCustomerInformation: (entity) => {
+    //payload {customer_id: 1}
+    const customer_id = {
+      customer_id: entity.customer_id
+    }
+    //delete customer_id to avoid update primary key
+    delete  entity.customer_id;
+    return db.patch(TABLE_NAME,entity,customer_id);
+  },
+  addNew: (entity) => {
+    return db.add(TABLE_NAME,entity);
+  },
 };
