7b44b29628113dcfc35b5eae943329f52417d100
t0dida00 <t0dida00@students.oamk.fi>
2022-03-29 01:39:46 +0300

    order model

--- models/order.model.js
+++ models/order.model.js
@@ -1,14 +1,33 @@
 var db = require('../utils/db');
 var config = require('../config/default.json')
-const TABLE_NAME= 'order';
+const TABLE_NAME= ` ${config.mysql.database}.order`;
+const TABLE_NAME2= `order_detail`;
 module.exports = {
   get: () => {
-    return db.load(`select * from ${config.mysql.database}.${TABLE_NAME}`);
+    
+    return db.load(`select * from ${TABLE_NAME} INNER JOIN customer ON customer.customer_id =${TABLE_NAME}.customer_id`);
   },
   add: (entity) => {
     return db.add(TABLE_NAME,entity);
   },
-  delete: (condition) => {
-    return db.del(TABLE_NAME,condition);
+  // delete: (condition) => {
+  //   return db.del(TABLE_NAME,condition);
+  // },
+  update: (entity) => {
+    const order_id = {
+      order_id: entity.order_id
+    }
+    return db.patch(TABLE_NAME,entity,order_id);
   },
+  getByCustomerID:  (customer_id) => {
+    
+    return db.load(`select * from ${TABLE_NAME} INNER JOIN customer ON customer.customer_id = ${TABLE_NAME}.customer_id where ${TABLE_NAME}.customer_id= ${customer_id} ` );
+  },
+  addDetail: (entity) => {
+    return db.add(TABLE_NAME2,entity);
+  },
+  getDetail:(order_id)=>{
+    return db.load(`select * from ${TABLE_NAME2} INNER JOIN product_detail ON product_detail.product_detail_id = order_detail.product_detail_id 
+    where order_id = ${order_id} `)
+  }
 };
