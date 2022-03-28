d718c77268946f494b87abed87e01b0cf9bec787
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2022-03-28 08:10:36 +0300

    #82 category model(get, get by id, add, edit, delete) done.

--- models/category.model.js
+++ models/category.model.js
@@ -1,8 +1,33 @@
-var db = require('../utils/db');
-const TABLE_NAME= 'category';
+var db = require("../utils/db");
+const TABLE_NAME = "category";
 module.exports = {
   getAllCategory: () => {
     return db.load(`select * from ${TABLE_NAME}`);
   },
+  all: function () {
+    return db.load(`select * from ${TABLE_NAME}`);
+  },
+  single: function (id) {
+    return db.load(`select * from ${TABLE_NAME} where category_id = ${id}`);
+  },
+  add: function (entity) {
+    return db.add(TABLE_NAME, entity);
+  },
+  patch: function (entity) {
+    const condition = {
+      category_id: entity.category_id,
+    };
 
+    delete entity.category_id;
+    return db.patch(TABLE_NAME, entity, condition);
+  },
+  del: function (id) {
+    const condition = {
+      category_id: id,
+    };
+    return db.del(TABLE_NAME, condition);
+  },
+  available: function (category_name) {
+    return db.load(`select * from ${TABLE_NAME} where category_name = "${category_name}"`);
+  },
 };
