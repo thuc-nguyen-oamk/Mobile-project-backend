44a3536467c261f7690cd10eba405c4473ec624d
t0dida00 <t0dida00@students.oamk.fi>
2022-03-31 01:07:05 +0300

    swagger

--- routes/banner.route.js
+++ routes/banner.route.js
@@ -2,11 +2,92 @@ const express = require("express");
 const banner = require("../models/banner.model");
 const router = express.Router();
 const passport = require("passport");
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Banner:
+ *       type: object
+ *       required:
+ *         - banner_image
+ *       properties:
+ *         id:
+ *           type: int
+ *           description: The auto-generated id of the banner
+ *         banner_image:
+ *           type: string
+ *           description: The banner image png/jpg
+ *       example:
+ *         banner_id: 1
+ *         banner_image: banner.png
+ */
+ /**
+  * @swagger
+  * tags:
+  *   name: Banner
+  *   description: The banner managing API
+  */
+/**
+ * @swagger
+ * /banner:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Banner]
+ *     responses:
+ *       200:
+ *         description: The list of the banners
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 bannerList:
+ *                      type: array
+ *                      items:
+ *                           $ref: '#/components/schemas/Banner'
+ *                 empty: 
+ *                      type: boolean
+ *                      example: false
+ *                       
+ *                   
+ */
+
 router.get('/', async function (req, res) {
     const list = await banner.getAll();
 
     res.send({bannerList: list, empty: list.length === 0})
 })
+
+
+/**
+ * @swagger
+ * /banner/add:
+ *   post:
+ *     summary: Add new a banner
+ *     tags: [Banner]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               banner_image: 
+ *                  type: string
+ *             example:
+ *                banner_image: banner2.png     
+ *     responses:
+ *       200:
+ *         description: The banner was successfully created
+ *       500:
+ *         description: Some server error
+ *      
+ *       401:
+ *         description: Unauthorized
+ */
+
+
 
 router.post('/add',passport.authenticate("jwt.admin", { session: false }), async function (req, res) {
     console.log(req.body)
@@ -14,9 +95,35 @@ router.post('/add',passport.authenticate("jwt.admin", { session: false }), async
     res.status(200).send("Add new voucher suceeded")
 })
 
-router.delete('/delete',passport.authenticate("jwt.admin", { session: false }), async function (req, res) {
-   
-    await banner.delete (req.body)
+
+/**
+ * @swagger
+ * /banner/delete/{id}:
+ *   delete:
+ *     summary: Remove the banner by id
+ *     tags: [Banner]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The banner id
+ * 
+ *     responses:
+ *       200:
+ *         description: The banner was deleted
+ *       404:
+ *         description: The banner was not found
+ *       401:
+ *         description: Unauthorized
+ */
+
+router.delete('/delete/:id',passport.authenticate("jwt.admin", { session: false }), async function (req, res) {
+   const condition = {
+       "banner_id":req.params.id
+   }
+    await banner.delete (condition)
     res.status(200).send("Delete new voucher suceeded")
 })
 
--- routes/customer.route.js
+++ routes/customer.route.js
@@ -8,6 +8,46 @@ var bcrypt = require('bcryptjs');
 const router = express.Router();
 
 let jwtSecretKeyLogin = config.authenticate.jwtSecret;
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Customer:
+ *       type: object
+ *       required:
+ *         - customer_email
+ *         - customer_name
+ *         - customer_address
+ *         - customer_phone
+ *       properties:
+ *         customer_id:
+ *           type: int
+ *           description: The auto-generated id of the customer
+ *         customer_email:
+ *           type: string
+ *           description: customer email
+ *         customer_name:
+ *           type: string
+ *           description: customer name
+ *         customer_address:
+ *           type: string
+ *           description: customer address
+ *         customer_phone:
+ *           type: string
+ *           description: customer phone
+ *       example:
+ *         customer_id: 1
+ *         customer_email: ddkhoa@gmail.com
+ *         customer_name: DINH DANG KHOA
+ *         customer_address: Kalervontie 
+ *         customer_phone: 123456789
+ */
+ /**
+  * @swagger
+  * tags:
+  *   name: Customer
+  *   description: The customer managing API
+  */
 
 router.post("/register", async (req, res, next) => {
   console.log('req.body.customer_password',req.body.customer_password)
@@ -50,10 +90,37 @@ router.post("/login", passport.authenticate("basic.customer", { session: false }
   res.json({ token });
 });
 
-
+/**
+ * @swagger
+ * /customers:
+ *   get:
+ *     summary: Returns the list of all the users
+ *     tags: [Customer]
+ *     responses:
+ *       200:
+ *         description: The list of the customers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userList:
+ *                      type: array
+ *                      items:
+ *                           $ref: '#/components/schemas/Customer'
+ *                 empty: 
+ *                      type: boolean
+ *                      example: false
+ *       401:
+ *         description: Unauthorized             
+ *                   
+ */
 router.get('/', passport.authenticate("jwt.admin", { session: false }),async function (req, res) {
   const list = await userModel.getAll();
-
+ for (i in list )
+ {
+   delete list[i].customer_password
+ }
   res.send({userList: list, empty: list.length === 0})
 })
 
@@ -63,10 +130,48 @@ router.get('/', passport.authenticate("jwt.admin", { session: false }),async fun
 
 //   res.send({userList: list, empty: list.length === 0})
 // })
+/**
+ * @swagger
+ * /customers/update:
+ *   post:
+ *     summary: Update customer information
+ *     tags: [Customer]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               customer_id:
+ *                  type: int
+ *               customer_email:
+ *                  type: string
+ *               customer_name:
+ *                  type: string
+ *               customer_address:
+ *                  type: string
+ *               customer_phone:
+ *                  type: string
+ *             example:
+ *                customer_id: 69
+ *                customer_email: ddkhoa@gmail.com
+ *                customer_name: DINH DANG KHOA
+ *                customer_address: Kalervontie
+ *                customer_phone: '123456789'   
+ *     responses:
+ *       200:
+ *         description: Update succeeded
+ *       500:
+ *         description: Some server error
+ *      
+ *       401:
+ *         description: Unauthorized
+ */
 
 router.post('/update', passport.authenticate("jwt", { session: false }), async function (req, res) {
   console.log(req.body)
-  // await banner.add(req.body)
+  // await customer.add(req.body)
  await userModel.updateCustomerInformation(req.body)
 
   res.status(200).send("Update succeeded")
