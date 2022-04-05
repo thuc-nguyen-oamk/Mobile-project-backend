0fae9d35c41d1e4dbb77516c8b8873e6a70126e8
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2022-04-05 13:41:06 +0300

    temporary save

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const imageUpload = require("./middlewares/upload");
-const userModel = require("./models/customer.model");
-var multer = require('multer');
-require("./middlewares/auth")
-const bodyParser = require("body-parser");
-const config = require("./config/default.json")
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('swagger-jsdoc');
+// const imageUpload = require("./middlewares/upload");
+// const userModel = require("./models/customer.model");
+// var multer = require('multer');
+// require("./middlewares/auth")
+// const bodyParser = require("body-parser");
+// const config = require("./config/default.json")
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('swagger-jsdoc');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 // app.get("/users", async (req, res) => {
 //   const list = await userModel.all();
@@ -23,34 +24,56 @@ app.get("/", (req, res) => {
 // });
 
 
-const specs = swaggerDocument(config.swagger_options);
+// const specs = swaggerDocument(config.swagger_options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.use('/products', require('./routes/product.route'))
-app.use('/customers', require('./routes/customer.route'))
-app.use('/admin', require('./routes/admin.route'))
-app.use('/voucher', require('./routes/voucher.route'))
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// app.use('/products', require('./routes/product.route'))
+// app.use('/customers', require('./routes/customer.route'))
+// app.use('/admin', require('./routes/admin.route'))
+// app.use('/voucher', require('./routes/voucher.route'))
 
-app.use('/categories', require('./routes/category.route'))
-app.use('/messages', require('./routes/message.route'))
-app.use('/banner', require('./routes/banner.route'))
-app.use('/order', require('./routes/order.route'))
-/************** UPLOAD IMAGE *************************/
-app.post("/uploadImage", (req, res) => {
+// app.use('/categories', require('./routes/category.route'))
+// app.use('/messages', require('./routes/message.route'))
+// app.use('/banner', require('./routes/banner.route'))
+// app.use('/order', require('./routes/order.route'))
+// /************** UPLOAD IMAGE *************************/
+// app.post("/uploadImage", (req, res) => {
 
- imageUpload.single("myImage")(req,res, function (err){
-    if (err instanceof multer.MulterError) {
-        console.log(err)
-      } else if (err) {
-        console.log(err)
-      }
-      else{
-       console.log("it's ok")
-      }
- })
+//  imageUpload.single("myImage")(req,res, function (err){
+//     if (err instanceof multer.MulterError) {
+//         console.log(err)
+//       } else if (err) {
+//         console.log(err)
+//       }
+//       else{
+//        console.log("it's ok")
+//       }
+//  })
 
-});
+// });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+// const server = require("http").createServer();
+
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", socket => {
+  console.log("a user connected :D");
+  socket.on("chat message", msg => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
+
+app.use(require("cors")())
+
+// server.listen(3000);
