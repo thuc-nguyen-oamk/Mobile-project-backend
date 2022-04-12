df11119f84f38c4cd44483d0a5eb2a8189a3ea85
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2022-04-12 09:56:17 +0300

    #107, #108: Chat feature almost done, still need some styling. Pushing for other members to test

--- utils/socketio.js
+++ utils/socketio.js
@@ -12,15 +12,17 @@ module.exports = function configSocketIO(server) {
 
   global.io.on("connection", async (socket) => {
     console.log("a user connected");
+
     socket.on("customer join", (data) => {
       console.log("data from client:", data);
-      const token = data.token;
+      let token = data.token;
+      token = token.replace(/"/g, "");
       const decoded = jwt.decode(token);
       console.log("decoded:", decoded);
 
-      if (!decoded && !decoded.customer_id) {
+      if (!decoded || !decoded.customer_id) {
         console.error(`Wrong token from ${socket.id}`);
-        io.to(socket.id).emit("force disconnect", { msg: "Access denied" });
+        io.to(socket.id).emit("force disconnect", { msg: "Unauthorized." });
         socket.disconnect(true);
         return;
       }
@@ -33,15 +35,43 @@ module.exports = function configSocketIO(server) {
         } else {
           console.log("messageList:", messageList);
           global.io.to(socket.id).emit("join", { messageList });
+          socket.join(customerId);
+        }
+      });
+    });
+
+    socket.on("admin join", (data) => {
+      console.log("data from admin client:", data);
+      let token = data.token;
+      token = token.replace(/"/g, "");
+      const decoded = jwt.decode(token);
+      console.log("decoded:", decoded);
+
+      if (!decoded || !decoded.admin_id) {
+        console.error(`Wrong token from ${socket.id}`);
+        io.to(socket.id).emit("force disconnect", { msg: "Unauthorized." });
+        socket.disconnect(true);
+        return;
+      }
+
+      const adminId = decoded.admin_id;
+      const customerId = data.customer_id
+      messageModel.getAllMessagesOfAConversation(adminId, customerId, (err, messageList) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("messageList:", messageList);
+          global.io.to(socket.id).emit("join", { messageList });
           socket.join(customerId)
         }
       });
     });
 
     socket.on("message", (data) => {
-      console.log(`message: ${data.msg}`);
-      global.io.in(data.room).emit("message", data.msg);
-      messageModel.add;
+      console.log('[socket on msg] data:', data);
+      global.io.in(data.room).emit("message", data);
+      const {sender_id, receiver_id, message_text} = data
+      messageModel.add({sender_id, receiver_id, message_text})
     });
 
     socket.on("disconnect", () => {
